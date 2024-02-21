0a4182b4e437bcfa93a7ca22b4032a651e5cdeb6
Emanuel Avila <avilaemanueel@gmail.com>
2024-02-21 10:31:08 -0300

    update to use string

--- backend/core/db/models/task.py
+++ backend/core/db/models/task.py
@@ -8,7 +8,7 @@ class Task(db.Model):
     task_id = db.Column(db.String(36), unique=True, default=uuid4)
     title = db.Column(db.String(255), nullable=True)
     description = db.Column(db.Text, nullable=True)
-    deadline = db.Column(db.DateTime, nullable=True)
+    deadline = db.Column(db.String(64), nullable=True)
     is_completed = db.Column(db.Boolean, default=False, nullable=True)
 
     def __init__(self, title, description=None, deadline=None, is_completed=False):
