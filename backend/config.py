6ce70905898a08afc2d9331be7bf6dad6a02f881
Emanuel Avila <avilaemanueel@gmail.com>
2024-02-21 22:53:03 -0300

    fix connection error on docker

--- backend/config.py
+++ backend/config.py
@@ -1,8 +1,3 @@
-# Enable debug mode.
-DEBUG = True
-
 # Secret key for session management. You can generate random strings here:
 # https://randomkeygen.com/
 SECRET_KEY = 'my secret'
-
-SQLALCHEMY_DATABASE_URI = 'postgresql://postgres:my-secure-password@localhost:5432/pid'
--- backend/core/db/connection.py
+++ backend/core/db/connection.py
@@ -1,10 +1,10 @@
-# core/db/connection.py
 from flask_sqlalchemy import SQLAlchemy
+import os
 
 db = SQLAlchemy()
 
 
 def configure_db(app):
-    # app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://username:password@localhost/dbname'
+    app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL', "postgresql://postgres:my-secure-password@localhost:5432/pid")
     app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
     db.init_app(app)
